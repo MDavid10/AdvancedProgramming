e20808bdec1e7c2ed3a824696d21589ccb3af79b
root <root@LAPTOP-8K3VTVF1.localdomain>
2021-10-13 22:54:49 -0500

    Four coding session Type Casting

---
+++ unit 1/utils.js
@@ -0,0 +1,12 @@
+var module = "Este es mi modulo!";
+export default class myUtils{
+    constructor(){   
+    };
+}
+export function validateWord(variable, palabra){
+    if(variable.includes(palabra)){
+        return true;
+    };
+    return false;
+    }
+//export {validateWord, module}; 
